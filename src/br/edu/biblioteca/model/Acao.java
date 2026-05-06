38e44eee106b70550918ba41f6ffb2be2dbebadf
Gabriel Tin <gtinseg@gmail.com>
2026-05-06 10:55:17 -0300

    Fase 2: Acao

---
+++ src/br/edu/biblioteca/model/Acao.java
@@ -0,0 +1,22 @@
+package br.edu.biblioteca.model;
+
+public class Acao {
+
+    private String tipo;
+    private String descricao;
+
+    public Acao(String tipo, String descricao) {
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public String toString() {
+        return "[" + tipo + "] " + descricao;
+    }
+}
